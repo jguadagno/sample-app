958c3bb6a7c28d048c846701b27e1995ed26452f
jguadagno <jguadagno@hotmail.com>
2023-06-10 10:17:20 -0700

    Moved to single 'program.cs' file

--- src/Contacts.Api/Program.cs
+++ src/Contacts.Api/Program.cs
@@ -1,15 +1,64 @@
-using Contacts.Api;
-using Microsoft.AspNetCore.Hosting;
+using System;
+using System.IO;
+using System.Reflection;
+using Contacts.Data;
+using Contacts.Data.SqlServer;
+using Contacts.Domain.Interfaces;
+using Contacts.Logic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
-CreateHostBuilder(args).Build().Run();
+var builder = WebApplication.CreateBuilder(args);
 
-IHostBuilder CreateHostBuilder(string[] args) =>
-Host.CreateDefaultBuilder(args)
-    .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
-    .ConfigureLogging(logging =>
-    {
-        logging.ClearProviders();
-        logging.SetMinimumLevel(LogLevel.Trace);
-    });
+// Add services to the container.
+builder.Services.AddTransient<IContactRepository, ContactRepository>();
+builder.Services.AddTransient<IContactManager, ContactManager>();
+builder.Services.AddTransient<IContactDataStore, SqlServerDataStore>();
+builder.Services.AddTransient<IConfiguration>(sp => builder.Configuration);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+// Register the Swagger generator, defining 1 or more Swagger documents
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "Contact API", 
+            Version = "v1",
+            Description = "The API for the Contacts Application",
+            TermsOfService = new Uri("https://example.com/terms"),
+            Contact = new OpenApiContact
+            {
+                Name = "Joseph Guadagno",
+                Email = "jguadagno@hotmail.com",
+                Url = new Uri("https://www.josephguadagno.net"),
+            }
+        });
+                
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- src/Contacts.WebUi/Program.cs
+++ src/Contacts.WebUi/Program.cs
@@ -1,26 +1,42 @@
-using Microsoft.AspNetCore.Hosting;
+using Contacts.WebUi.Models;
+using Contacts.WebUi.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace Contacts.WebUi
+var builder = WebApplication.CreateBuilder(args);
+
+var settings = new Settings();
+builder.Configuration.Bind("Settings", settings);
+builder.Services.AddSingleton(settings);
+
+// Add services to the container.
+builder.Services.AddTransient<IContactService, ContactService>();
+builder.Services.AddHttpClient();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddApplicationInsightsTelemetry();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.SetMinimumLevel(LogLevel.Trace);
-                });
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
